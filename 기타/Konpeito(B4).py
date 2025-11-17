d10f5f3a4bb2704e9bf2590a371e5cf72d66b005
Juyear009 <githubbruny@gmail.com>
2025-11-17 20:09:07 +0900

    2025-11-17 solved: 4

---
+++ 기타/Konpeito(B4).py
@@ -0,0 +1,2 @@
+nums = map(int,input().split())
+print(max(nums)-nums[0] + max(nums)-nums[1] + max(nums)-nums[2])
---
+++ 기타/대전과학고등학교(B5).py
@@ -0,0 +1,10 @@
+while True:
+  w = input()
+  if w == "animal":
+    print("Panthera tigris")
+  elif w == "flower":
+    print("Forsythia koreana")
+  elif w == "tree":
+    print("Pinus densiflora")
+  elif w == "end":
+    break

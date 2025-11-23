ee25e0e3f4501da1877d8651b2ed5e222234d4fb
Juyear009 <githubbruny@gmail.com>
2025-11-23 19:41:00 +0900

    2025-11-23 solved: 1

--- 동적 계획법 1/전깃줄(G5).py
+++ 동적 계획법 1/전깃줄(G5).py
@@ -4,9 +4,14 @@ input = sys.stdin.readline
 
 N = int(input())
 edges = [list(map(int,input().split())) for _ in range(N)]
+edges.sort()
 
-dp = [0] * N
+dp = [1] * N
 
 for i in range(1,N):
+    t1 = 0
     for j in range(i):
-        pass
+        if edges[j][1] < edges[i][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(N-max(dp))

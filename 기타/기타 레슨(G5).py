7030ce1295e9ca72a47812b2571a217f35ec5986
Juyear009 <githubbruny@gmail.com>
2025-12-02 21:24:09 +0900

    2025-12-02 solved: 1

--- 기타/기타 레슨(G5).py
+++ 기타/기타 레슨(G5).py
@@ -1,20 +1,35 @@
 def check(m):
     result = []
     t = 0
+    if max(nums) > m:
+        return False
     for i in nums:
         if t + i > m:
             result.append(t)
-            t = i
-        else:
-            t += i
+            t = 0
+        t += i
+    if t != 0:
+        result.append(t)
+    if len(result) > M:
+        return False
+    else:
+        return True
 
 def binary_search():
     left = 1
     right = sum(nums)
     while left <= right:
         mid = (left + right) // 2
-        i
+        if check(mid):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
 
 N,M = map(int,input().split())
-
 nums = list(map(int,input().split()))
+
+if N == M:
+    print(max(nums))
+else:
+    print(binary_search())

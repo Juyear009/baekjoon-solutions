341df1f46598eaf0a2fffe94839a6047244762fe
Juyear009 <githubbruny@gmail.com>
2025-12-31 17:12:41 +0900

    2025-12-31 solved: 3

---
+++ 트리/사회망 서비스(SNS)(G3).py
@@ -0,0 +1,37 @@
+import sys
+
+def dfs():
+  stack = [(1,0,False)]
+  while stack:
+    node,parent,check = stack.pop()
+    if not check:
+      stack.append((node,parent,True))
+      if tree[node]:
+        for newNode in tree[node]:
+          if newNode != parent:
+            stack.append((newNode,node,False))
+    else:
+      if len(tree[node]) == 1 and tree[node][0] == parent:
+        dp[node][1] = 1
+      else:
+        for child in tree[node]:
+          if child != parent:
+            dp[node][0] += dp[child][1]
+            dp[node][1] += min(dp[child][0], dp[child][1])
+        dp[node][1] += 1
+
+
+input = sys.stdin.readline
+
+N = int(input())
+tree = [[] for _ in range(N+1)]
+dp = [[0,0] for _ in range(N+1)]
+
+for _ in range(N-1):
+  u,v = map(int,input().split())
+  tree[u].append(v)
+  tree[v].append(u)
+
+dfs()
+print(min(dp[1]))
+

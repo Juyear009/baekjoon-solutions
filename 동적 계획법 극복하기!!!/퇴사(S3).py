268ed58373f962696aad3dde0a34bd0275e7f344
Juyear009 <githubbruny@gmail.com>
2026-01-11 00:31:21 +0900

    2026-01-11 solved: 2

--- 동적 계획법 극복하기!!!/퇴사(S3).py
+++ 동적 계획법 극복하기!!!/퇴사(S3).py
@@ -1,26 +1,12 @@
 N = int(input())
 schedule = [list(map(int,input().split())) for _ in range(N)]
 
-dp = [[0 for _ in range(N)] for _ in range(N)]
+dp = [0 for _ in range(N+1)]
 
 for i in range(N):
-  for j in range(i,schedule[i][0]+i):
-    if schedule[i][0] + i > N:
-      continue
-    dp[i][j] = schedule[i][1]
-
-for i in range(1,N):
-  for j in range(i):
-    if dp[j][i] == 0:
-      for k in range(i,schedule[i][0]+i):
-        if schedule[i][0] + i > N:
-          continue
-        dp[i][k] = max(dp[i][k],max(dp[j])+schedule[i][1])
+  dp[i+1] = max(dp[i+1],dp[i])
+  if i + schedule[i][0] <= N:
+    dp[i+schedule[i][0]] = max(dp[i+schedule[i][0]],dp[i]+schedule[i][1])
 
 
-res = 0
-for i in dp:
-  for j in i:
-    res = max(res,j)
-
-print(res)
+print(dp[N])

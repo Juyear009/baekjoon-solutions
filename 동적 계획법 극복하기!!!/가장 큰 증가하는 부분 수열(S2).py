4766632003bd2cd3abd4f0031cbbf7b734775a1f
Juyear009 <githubbruny@gmail.com>
2026-01-20 18:28:54 +0900

    2026-01-20 solved: 1

--- 동적 계획법 극복하기!!!/가장 큰 증가하는 부분 수열(S2).py
+++ 동적 계획법 극복하기!!!/가장 큰 증가하는 부분 수열(S2).py
@@ -4,9 +4,7 @@ dp = [nums[i] for i in range(N)]
 
 for i in range(1,N):
   for j in range(i):
-    print(dp)
     if nums[j] < nums[i]:
       dp[i] = max(dp[i], dp[j]+nums[i])
 
-print(dp)
 print(max(dp))
